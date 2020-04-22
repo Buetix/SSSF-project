67a7d690dd38b5771a0c6b6e934e9c8644bae656
Lassi Ylirautalahti <lassiy@metropolia.fi>
2020-04-22 21:30:56 +0300

    Added db file and connection to mongodb

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 ﻿'use strict';
 
+require('dotenv').config();
 const express = require('express');
+const db = require('./models/db');
 const app = express();
+const cors = require('cors');
+
+app.use(cors);
 
 app.get('/', (req, res) => {
    res.send('Hello world') 
 });
 
-app.listen(3000, () => {
-   console.log('App listening on port 3000') 
+db.on('connected', () => {
+   app.listen(3000, () => console.log(`Example app listening on port 3000!`));
 });
