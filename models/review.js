bd3921ce1f4a8cbd30c8f086546a00758041ec90
Lassi Ylirautalahti <lassiy@metropolia.fi>
2020-04-26 19:35:53 +0300

    Db model for review and list

---
+++ models/review.js
@@ -0,0 +1,12 @@
+﻿'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const review = new Schema ({
+    movieTitle: String,
+    moviePoster: String,
+    comment: String
+});
+
+module.exports = mongoose.model('Review', review);
---
+++ models/topList.js
@@ -0,0 +1,12 @@
+﻿'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const topList = new Schema ({
+    position: Number,
+    review: [{type: mongoose.Types.ObjectId, ref: 'Review'}],
+    comment: String
+});
+
+module.exports = mongoose.model('TopList', topList);
